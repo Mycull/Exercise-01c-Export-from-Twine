1dca29f4c8b8e2c0a64ae5377b18f6998fd9a1d3
Mycull <82784090+Mycull@users.noreply.github.com>
2021-09-02 01:12:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -230,3 +230,22 @@ if "name" in world and "passages" in world:
         for link in passage["links"]:
             print(link["linkText"])
         print()
+        
+"""
+# Exercise-01c-Export-from-Twine
+Exercise for MSCH-C220, 31 August 2021
+
+A Python dictionary, describing an interactive fiction game, exported from Twine
+
+## Implementation
+Created using Twine 2 and exported with [Harlowe 3 to JSON 0.0.6 by Jonathan Schoonhoven](https://github.com/jtschoonhoven/twine-to-json)
+
+## References
+[Zork, 1977 by Tim Anderson, Marc Blank, Dave Lebling, and Bruce Daniels](https://en.wikipedia.org/wiki/Zork)
+
+## Future Development
+None
+
+## Created by
+Michael Truong
+"""
